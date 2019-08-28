6ba578d8f069f9a7fbee87b4e667a1fdb425b311
César Souza Liedke <cesar.liedke@corp.globo.com>
2019-08-28 16:04:25 -0300

    feat: refactor some of the base character class

--- packages/character.py
+++ packages/character.py
@@ -7,85 +7,94 @@ import stealth
 
 class Char:
     def __init__(self):
-        self._name: str = None
         self._ability: Dict[str, int] = {}
         self._dead: bool = None
+        self._name: str = None
         self._position: Dict[str, int]
         self._resist: Dict[str, int] = {}
         self._skills: Dict[str, int] = {}
         self._stats: Dict[str, int] = {}
 
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @property
-    def ability(self) -> Dict[str, int]:
-        return self._ability
-
-    @property
-    def dead(self) -> bool:
-        return self._dead
-
-    @property
-    def position(self) -> Dict[str, int]:
-        return self._position
-
-    @property
-    def resist(self) -> Dict[str, int]:
-        return self._resist
-
-    @property
-    def skills(self) -> Dict[str, int]:
-        return self._skills
-
-    @property
-    def stats(self) -> Dict[str, int]:
-        return self._stats
-
 
 class MyChar(Char):
     def __init__(self):
+        super(MyChar, self).__init__()
+        self._ability["dex"]: int = 0
+        self._ability["int"]: int = 0
+        self._ability["str"]: int = 0
         self._armor: int = None
         self._backpack: int = None
+        self._connected_time: str = ""
+        self._dead: bool = None
+        self._disconnected_time: str = ""
+        self._extended_info: Dict[str, str] = {}
         self._name: str = None
         self._title: str = None
         self._resist["cold"]: int = None
         self._resist["energy"]: int = None
         self._resist["fire"]: int = None
         self._resist["poison"]: int = None
-        self._connected_time: str = stealth.ConnectedTime()
-        self._dead: bool = stealth.Dead()
-        self._ability["dex"]: int = stealth.Dex()
-        self._disconnected_time: str = stealth.DisconnectedTime()
-        self._resist["energy"]: str = stealth.EnergyResist()
-        self._extended_info: Dict[str, str] = stealth.GetExtInfo()
 
     @property
-    def armor(self):
+    def ability(self, prop=None) -> int:
+        logging.info(f"Getting character ability value for {prop}")
+        if prop:
+            if prop == "dex":
+                self._resist[prop] = stealth.ColdResist()
+            elif prop == "int":
+                self._resist[prop] = stealth.EnergyResist()
+            elif prop == "str":
+                self._resist[prop] = stealth.FireResist()
+            else:
+                logging.info(f"Not a valid ability option")
+            return self._resist[prop]
+        logging.info(f"Try passing dex, int or str as a parameter")
+        return None
+
+    @property
+    def armor(self) -> int:
         logging.info(f"Getting armor value")
         self._armor = stealth.Armor()
         return self._armor
 
     @property
-    def backpack(self):
+    def backpack(self) -> int:
         logging.info(f"Getting backpack id")
         self._backpack = stealth.Backpack()
         return self._backpack
 
     @property
-    def name(self):
+    def connected_time(self) -> str:
+        logging.info(f"Getting connected time")
+        self._connected_time = stealth.ConnectedTime()
+        return self._connected_time
+    
+    @property
+    def dead(self) -> bool:
+        logging.info(f"Getting dead property")
+        self._dead = stealth.Dead()
+        return self._dead
+
+    @property
+    def extended_info(self) -> Dict[str, str]:
+        logging.info(f"Getting character extended info")
+        self._extended_info = stealth.GetExtInfo()
+        return self._extended_info
+
+    @property
+    def name(self) -> str:
         logging.info(f"Getting character name")
         self._name = stealth.CharName()
         return self._name
 
     @property
-    def title(self):
+    def title(self) -> str:
         logging.info(f"Getting character title")
         self._title = stealth.CharTitle()
+        return self._title
 
     @property
-    def resist(self, prop=None):
+    def resist(self, prop=None) -> int:
         logging.info(f"Getting character resistance for {prop}")
         if prop:
             if prop == "cold":
